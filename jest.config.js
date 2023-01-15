51e699f1a1fe8a444e22ad1750c23e82bda1cd80
Gabriel Ferraro <pardal275@hotmail.com>
2023-01-15 14:51:20 -0300

    feat:Insertion of coverage to jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coverageReporters: ['text', 'cobertura'],
+  reporters: ['default', 'jest-junit'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
